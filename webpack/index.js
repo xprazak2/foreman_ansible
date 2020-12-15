f026a603ea544ff0fa0ef571bed0325f6ea79b9a
Shira Maximov <shiramaximov@gmail.com>
2020-12-15 15:56:51 +0200

    Import roles and veriables

--- webpack/index.js
+++ webpack/index.js
@@ -3,6 +3,7 @@ import componentRegistry from 'foremanReact/components/componentRegistry';
 import injectReducer from 'foremanReact/redux/reducers/registerReducer';
 import ReportJsonViewer from './components/ReportJsonViewer';
 import AnsibleRolesSwitcher from './components/AnsibleRolesSwitcher';
+import ImportRolesAndVariablesTable from './components/AnsibleRolesAndVariables'
 import reducer from './reducer';
 
 const { tfm } = window;
@@ -14,6 +15,11 @@ componentRegistry.register({
 componentRegistry.register({
   name: 'AnsibleRolesSwitcher',
   type: AnsibleRolesSwitcher,
+});
+
+componentRegistry.register({
+    name: 'ImportRolesAndVariablesTable',
+    type: ImportRolesAndVariablesTable,
 });
 
 injectReducer('foremanAnsible', reducer);
